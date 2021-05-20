24d9f6e6ea0ecdf6ea8c2f46f518b638126c1cdd
LarisaSm <smirnova.larisa2020@yandex.ru>
2021-05-20 13:14:12 +0300

    fix env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,6 @@
 // middlewares/auth.js
 const jwt = require('jsonwebtoken');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 const ForbiddenError = require('../errors/ForbiddenError');
@@ -14,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new ForbiddenError('Нет доступа');
   }
