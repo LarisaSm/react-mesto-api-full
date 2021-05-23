c64d971e0fb918c49a550e42a62ccf29eea48fa3
LarisaSm <smirnova.larisa2020@yandex.ru>
2021-05-23 15:43:34 +0300

    fix joi router/cards

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -24,7 +24,7 @@ cardRouter.post('/cards', auth, celebrate({
       }
       return helpers.message('Невалидная ссылка');
     }),
-    name: Joi.string().require().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), createCard);
 
